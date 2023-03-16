ceecd993132e633dadd707878dd0fdbb49cf4191
조송하 <102511282+Song-haJo@users.noreply.github.com>
2023-03-17 00:23:02 +0900

    Update MainWindow.xaml.cpp

--- 06장/실습/06-1/MainWindow.xaml.cpp
+++ 06장/실습/06-1/MainWindow.xaml.cpp
@@ -10,7 +10,7 @@ using namespace Microsoft::UI::Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace winrt::App1::implementation
+namespace winrt::printImg::implementation
 {
     MainWindow::MainWindow()
     {
