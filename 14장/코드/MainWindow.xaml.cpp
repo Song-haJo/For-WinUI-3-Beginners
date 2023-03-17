0a4a4b5d1fe8ccc0456c16d9213df513d2856f1a
Mpicea <100979640+Mpicea@users.noreply.github.com>
2023-03-17 20:02:23 +0900

    Add files via upload

---
+++ 14장/코드/MainWindow.xaml.cpp
@@ -0,0 +1,120 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::ArtPen::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+
+        flag = false;
+        px = 100;
+        py = 100;
+        mySize = 16;
+        IsChecked = true;
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    /*void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }*/
+}
+
+struct winrt::Windows::UI::Color myCol = winrt::Microsoft::UI::Colors::Green();
+void winrt::ArtPen::implementation::MainWindow::Slider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e)
+{
+    mySize = e.NewValue();
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::CanvasControl_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    flag = true;
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::CanvasControl_PointerReleased(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    flag = false;
+    px = py = 0.0;
+    vx.push_back(px);
+    vy.push_back(py);
+    col.push_back(myCol);
+    size.push_back(mySize);
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    px = e.GetCurrentPoint(canvas).Position().X;
+    py = e.GetCurrentPoint(canvas).Position().Y;
+    if (flag) {
+        vx.push_back(px);
+        vy.push_back(py);
+        col.push_back(myCol);
+        size.push_back(mySize);
+        canvas.Invalidate();
+    }
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    int n = vx.size();
+    if (n <= 2) return;
+
+    for (int i = 1; i < n; i++) {
+        if (vx[i] == 0.0 && vy[i] == 0.0) {
+            i++; continue;
+        }
+        args.DrawingSession().DrawLine(vx[i - 1], vy[i - 1], vx[i], vy[i], col[i], size[i]);
+        args.DrawingSession().FillCircle(vx[i - 1], vy[i - 1], size[i] / 2, col[i]);
+        args.DrawingSession().FillCircle(vx[i], vy[i], size[i] / 2, col[i]);
+    }
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::ColorPicker_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    if (IsChecked)
+    {
+        IsChecked = FALSE;
+        ColorPicker().Label(L"Disable");
+
+        colorPanel().Visibility(Visibility::Collapsed);
+    }
+    else
+    {
+        IsChecked = TRUE;
+        ColorPicker().Label(L"Enable");
+
+        colorPanel().Visibility(Visibility::Visible);
+    }
+}
+
+
+void winrt::ArtPen::implementation::MainWindow::ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args)
+{
+    myCol = args.NewColor();
+}
---
+++ 14장/코드/MainWindow.xaml.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+#include <winrt/Microsoft.UI.Input.h>
+
+using namespace winrt::Microsoft::UI;
+using namespace winrt::Microsoft::Graphics::Canvas::UI::Xaml;
+
+namespace winrt::ArtPen::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        bool flag;
+        bool IsChecked;
+        float px;
+        float py;
+        float mySize;
+        std::vector<float> vx;
+        std::vector<float> vy;
+        std::vector<struct winrt::Windows::UI::Color> col;
+        std::vector<float> size;
+
+        //void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void Slider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e);
+        void CanvasControl_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_PointerReleased(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+        void ColorPicker_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args);
+    };
+}
+
+namespace winrt::ArtPen::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
