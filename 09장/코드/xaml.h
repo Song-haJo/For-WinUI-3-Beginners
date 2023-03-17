67a93bff17f51e0005e457017c0a7e78b2d6e8d7
chaerin-Github <105805974+chaerin-Github@users.noreply.github.com>
2023-03-17 21:50:23 +0900

    Create xaml.h

---
+++ 09장/코드/xaml.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::Browser::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void MenuFlyoutItem_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void get_button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::Browser::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
