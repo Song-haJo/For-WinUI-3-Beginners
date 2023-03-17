a18f24ead74bad23df52e3cbe18ae3ace06da50b
Mpicea <100979640+Mpicea@users.noreply.github.com>
2023-03-17 19:57:29 +0900

    Create 실습 13-3_MainWindow.xaml.h

---
+++ 13장/코드/실습 13-3_MainWindow.xaml.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Input.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+
+using namespace winrt::Microsoft::UI;
+using namespace winrt::Microsoft::Graphics::Canvas::UI::Xaml;
+
+namespace winrt::SimplePen::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        bool flag;
+        float px, py;
+        std::vector<float> vx;
+        std::vector<float> vy;
+
+        //void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+        void CanvasControl_PointerPressed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_PointerReleased(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+    };
+}
+
+namespace winrt::SimplePen::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
