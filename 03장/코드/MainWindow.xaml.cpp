5a951347c1d8f3363c037c127aee4b0ea92ae4c9
조송하 <102511282+Song-haJo@users.noreply.github.com>
2023-03-18 12:20:42 +0900

    Update MainWindow.xaml.cpp

--- 03장/코드/MainWindow.xaml.cpp
+++ 03장/코드/MainWindow.xaml.cpp
@@ -2,27 +2,24 @@
 #include "MainWindow.xaml.h"
 #if __has_include("MainWindow.g.cpp")
 #include "MainWindow.g.cpp"
-
 #endif
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-#include<microsoft.ui.xaml.window.h>
-namespace winrt::caculator::implementation
+#include <microsoft.ui.xaml.window.h>
+namespace winrt::Calculator::implementation
 {
-     MainWindow::MainWindow()
+    MainWindow::MainWindow()
     {
         InitializeComponent();
         auto windowNative{ this->try_as<::IWindowNative>() };
         HWND hWnd{ 0 };
         windowNative->get_WindowHandle(&hWnd);
-        SetWindowPos(hWnd, NULL, 300, 500, 550, 550, NULL); //크기
-        this->Title(L"Caculator"); // 창 제목
+        SetWindowPos(hWnd, NULL, 300, 500, 250, 300, NULL);     // 창 크기
+        this->Title(L"Calculator");                             // 창 제목
     }
+
     int32_t MainWindow::MyProperty()
     {
         throw hresult_not_implemented();
@@ -32,39 +29,41 @@ namespace winrt::caculator::implementation
     {
         throw hresult_not_implemented();
     }
-    using namespace std;
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        double a, b, c;
-        a = stod(va().Text().c_str());
-        b = stod(vb().Text().c_str());
-        c = a + b;
-        vc().Text(to_wstring(c));
-    }
+
 }
-void winrt::caculator::implementation::MainWindow::myButton1_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+using namespace std;
+double va, vb, vc;
+void winrt::Calculator::implementation::MainWindow::add_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    double a, b, c;
-    a = stod(ca().Text().c_str());
-    b = stod(cb().Text().c_str());
-    c = a - b;
-    cc().Text(to_wstring(c));
+    va = stod(a().Text().c_str());
+    vb = stod(b().Text().c_str());
+    vc = va + vb;
+    c().Text(to_wstring(vc));
 }
 
-void winrt::caculator::implementation::MainWindow::myButton2_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+
+void winrt::Calculator::implementation::MainWindow::subtract_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    double a, b, c;
-    a = stod(qa().Text().c_str());
-    b = stod(qb().Text().c_str());
-    c = a * b;
-    qc().Text(to_wstring(c));
+    va = stod(a().Text().c_str());
+    vb = stod(b().Text().c_str());
+    vc = va - vb;
+    c().Text(to_wstring(vc));
 }
 
-void winrt::caculator::implementation::MainWindow::myButton3_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+
+void winrt::Calculator::implementation::MainWindow::multiply_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    double a, b, c;
-    a = stod(ra().Text().c_str());
-    b = stod(rb().Text().c_str());
-    c = a / b;
-    rc().Text(to_wstring(c));
+    va = stod(a().Text().c_str());
+    vb = stod(b().Text().c_str());
+    vc = va * vb;
+    c().Text(to_wstring(vc));
+}
+
+
+void winrt::Calculator::implementation::MainWindow::divide_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+    va = stod(a().Text().c_str());
+    vb = stod(b().Text().c_str());
+    vc = va / vb;
+    c().Text(to_wstring(vc));
 }
